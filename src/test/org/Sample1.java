7ccb5695ea641965ef632781f514b96693b3fb59
SugumarB <sugubsk@gmail.com>
2024-05-07 19:32:15 +0530

    new code

---
+++ src/test/org/Sample1.java
@@ -0,0 +1,10 @@
+package test.org;
+
+public class Sample1 {
+public static void main(String[] args) {
+	System.out.println("ragu");
+	System.out.println("ram");
+	System.out.println("kathir");
+	System.out.println("vivi");
+}
+}
