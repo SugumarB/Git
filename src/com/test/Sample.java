5dcaba1e969c2aaa25ae4b76e8d21abd9099e85b
SugumarB <sugubsk@gmail.com>
2024-07-11 13:57:13 +0530

    Codes

---
+++ src/com/test/Sample.java
@@ -0,0 +1,20 @@
+package com.test;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+	}
+
+	private void Name() {
+		System.out.println("hari");
+		System.out.println("praveen");
+	}
+
+	private void Password() {
+		System.out.println("hari@123");
+		System.out.println("praveen@123");
+	}
+}
---
+++ src/com/test/Sample1.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class Sample1 {
+public static void main(String[] args) {
+	System.out.println("ragu");
+	System.out.println("ram");
+	System.out.println("kathir");
+	System.out.println("vivi");
+}
+}
